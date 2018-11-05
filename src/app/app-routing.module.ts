8925dc5169aa230f4136d324c1cebb7894f23e79
Abdulaziz <abdulmoha786@gmail.com>
2018-11-05 17:07:19 +0300

    routing tweaks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,19 @@ import { EditServerComponent } from "./servers/edit-server/edit-server.component
 import { UsersComponent } from "./users/users.component";
 import { UserComponent } from "./users/user/user.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { AuthGuard } from "./auth-guard.service";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 
 const appRoutes: Routes = [
     {path: '',component: HomeComponent },
-    {path: 'servers',component: ServersComponent, children: [
+    {path: 'servers',
+    // canActivate: [AuthGuard],
+    canActivateChild: [AuthGuard],
+    component: ServersComponent,
+
+    children: [
       {path: ':id', component: ServerComponent },
-      {path: ':id/edit',component: EditServerComponent },
+      {path: ':id/edit',component: EditServerComponent, canDeactivate: [CanDeactivateGuard]},
     ]},
     {path: 'users',component: UsersComponent, children: [
       {path: ':id/:name',component: UserComponent }
